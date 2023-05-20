6949d6c470b75ef34fa520d236eda987e5d7a3d6
Lokesh Yadav <108071094+lokeshyadav14@users.noreply.github.com>
2023-05-20 21:39:46 +0530

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,137 @@
+/*---------------------------- Home Section----------------------------------------*/
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+/*------------------- End Home Section----------------------------------------*/
+
+/*---------------------------- Service Section----------------------------------------
+
+const submit_button = document.querySelector(".button");
+submit_button.onclick = (e) => {
+    e.preventDefault();
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+    const cpass = document.getElementById("cpass").value;
+
+    localStorage.setItem('FirstName', fname);
+    localStorage.setItem('LastName', lname);
+    localStorage.setItem('Email', email);
+    localStorage.setItem('Password', pass);
+    localStorage.setItem('Cpassword', cpass);
+    if(fname == "" && lname == "" && email == "" && pass == "" && cpass == ""){
+        Swal.fire(
+            'Opps..!',
+            'input field has no value!',
+            'error'
+        );
+    }
+    else
+    {
+        if(pass.length >= 6 && pass.length <= 20)
+    {
+        if( pass !== cpass){
+            Swal.fire(
+                'Opps..!',
+                'Password not matching!',
+                'error'
+            );
+        }
+        else
+        {
+            Swal.fire(
+                'Good job!',
+                'Register successful!',
+                'success'
+            );
+            setTimeout(()=>{
+                    location.href='LoginBox.html';
+                    },5000)
+        }
+    }
+    else
+    {
+        Swal.fire(
+            'Opps..!',
+            'Input mim six digit password!',
+            'error'
+        );
+    }
+    }
+
+
+}
+
+
+----------------------------End Service Section----------------------------------------*/
+
+
+
+/*------------------- Member Section----------------------------------------*/
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
+
+/*-------------------End Member Section----------------------------------------*/
+
+
---
+++ static/readme.txt
@@ -0,0 +1,7 @@
+CSS, JS and images files
+
+* style contains css of index.html file.
+* userstyle contains css of user.html file.
+* app contains js of index.html file.
+* babelrc, plugins, dist contains css, bootstrap and js files for member and user dashboards.
+* img folder contains required images for website.
